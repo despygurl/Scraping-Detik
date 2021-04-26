187a2b139c9c047926d1d399d8bdfae62a5fd551
rarizmalia <amel025@gmail.com>
2021-04-26 14:16:45 +0700

    Scraping detail dan isi berita

--- detik/detik/spiders/detik_scrape.py
+++ detik/detik/spiders/detik_scrape.py
@@ -8,13 +8,39 @@ class PostsSpider(scrapy.Spider):
 
     ]
 
-    def parse(self, response):
-        for post in response.css('.list-content__item'):
-            yield {
-                'judul berita': post.css('.media__title a::text').get(),
-                'link berita': post.css('.media__title a::attr(href)').get(),
-                'gambar': post.css('img ::attr(src)').get(),
+    def parse_item(self,response):
+        pic = response.css('.detail__media-image img::attr(src)').get()
+        if pic is None:
+            item = {
+                "Judul": response.css('.detail__title::text').get().strip(),
+                'Author': response.css('.detail__author::text').get().replace(' - detikNews',''),
+                'Topik': response.css('div.nav a::text').getall(),
+                'Waktu' : response.css('.detail__date::text').get(),
+                'Gambar' : "Gambar tidak tersedia",
+                'Isi Berita': "".join(response.css('.detail__body-text.itp_bodycontent p::text').getall())
+
+
             }
+        else:
+            item = {
+                "Judul": response.css('.detail__title::text').get().strip(),
+                'Author': response.css('.detail__author::text').get().replace(' - detikNews', ''),
+                'Topik': response.css('div.nav a::text').getall(),
+                'Waktu': response.css('.detail__date::text').get(),
+                'Gambar': response.css('.detail__media-image img::attr(src)').get(),
+                'Isi Berita': "".join(response.css('.detail__body-text.itp_bodycontent p::text').getall())
+
+            }
+        yield item
+
+    def parse(self, response):
+        for post in response.css('h3'):
+            dict_url = {
+                'url': post.css('a::attr(href)').get()
+            }
+            link = dict_url.get('url')
+            if link is not None:
+                yield scrapy.Request(url=link, callback=self.parse_item)
 
             next_page = response.css('div.pagination.text-center.mgt-16.mgb-16 ::attr(href)')[-1].get()
             if next_page is not None:
--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
+import scrapy
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class DokumenSpider(scrapy.Spider):
+    name = 'dokumen'
+    allowed_urls = ['https://jdih.bkn.go.id/']
+    start_urls = ['https://jdih.bkn.go.id/dokumen/peraturan']
 
+    def parse_item(self, response):
+        #doklink = "https://jdih.bkn.go.id"
+        item =  {
+                "Judul": response.css('h5::text').geta(),
+                # "Tempat Terbit": response.css('.text-extra-dark-gray.font-weight-600::text')[0].get(),
+                # "Tanggal Penetapan": response.css('.text-extra-dark-gray.font-weight-600::text')[1].get(),
+                # "Tanggal Pengundangan": response.css('.text-extra-dark-gray.font-weight-600::text')[2].get(),
+                # "Sumber": response.css('.text-extra-dark-gray.font-weight-600::text')[3].get(),
+                # "Urusan Pemerintahan": response.css('.text-extra-dark-gray.font-weight-600::text')[4].get(),
+                # "Bidang Hukum": response.css('.text-extra-dark-gray.font-weight-600::text')[5].get(),
+                # "Bahasa": response.css('.text-extra-dark-gray.font-weight-600::text')[6].get(),
+                # "Pemrakarsa": response.css('.text-extra-dark-gray.font-weight-600::text')[7].get(),
+                # "Penandatanganan": response.css('.text-extra-dark-gray.font-weight-600::text')[8].get(),
+                # "Peraturan Terkait": response.css('.text-extra-dark-gray.font-weight-600::text')[10].get(),
+                # "Dokument Terkait": response.css('.text-extra-dark-gray.font-weight-600::text')[12].get(),
+                # "Hasil Uji Materi": response.css('.text-extra-dark-gray.font-weight-600::text')[14].get(),
+                # "Nama Pengarang": response.css('td::text')[0].get(),
+                # "Tipe Pengarang": response.css('td::text')[1].get(),
+                # "Jenis Pengarang": response.css('td::text')[2].get(),
+                # "Jenis Dokumen": response.css('strong::text')[0].get(),
+                # "Status": response.css('strong::text')[1].get(),
+                # "Lampiran": doklink + response.css('.widget-list a::attr(href)').get()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        }
+        yield item
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def parse(self, response):
+        for post in  response.css('h3'):
+            dict_url = {
+                'url': post.css('a::attr(href)').get()
+            }
+            link = dict_url.get('url')
+            if link is not None:
+                yield scrapy.Request(url=link, callback=self.parse_item)
